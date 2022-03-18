78c660330b6a89b1acfc12442a124c1c17a5e507
Christian <christian@infobaleen.com>
2022-03-18 22:51:48 +0100

    add boilerplate

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ajzo90/go-requests
+
+go 1.18
+
+require github.com/matryer/is v1.4.0 // indirect
---
+++ requests_test.go
@@ -0,0 +1,10 @@
+package requests
+
+import (
+	"github.com/matryer/is"
+	"testing"
+)
+
+func TestX(t *testing.T) {
+	is.New(t).NoErr(nil)
+}
